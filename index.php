0ebea699703791a63d57008cee8a5821ab52e3a6
Eyvor <104306808+Eyvor@users.noreply.github.com>
2022-04-28 09:04:32 +0300

    test

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+</head>
+<body>
+    <?php
+    /* define(my_age, 18);
+     echo my_age.'<BR>';
+
+    echo "<b>hellol</b> world";
+    echo "<br> Hellol world";
+    echo "<br>\"Hellol world\"";
+    echo '<br>\'hellol world\' ';
+    echo '<br>"hellol world"';
+*/
+   // комментарий 
+   # комментарий
+   /* комментарий*/
+     /* 
+     $(переменная) name ( название переменной )
+     */
+    /*$number = 5; //int
+    //$number = 45;
+    $num = -0.55; //float
+    echo "<br>Переменная:".$number . $num;
+    $str = "<br>Переменаая"; //string
+    $bool=false; // boolean
+    echo $str . ' : ' . $number .'. Var 2 : '. $num;
+    $a=0.5;
+    $b="0.5";
+     //echo $a + $b;
+    echo $a + floatval($b);
+    */
+   $x = 20;
+    $y = 40;
+    echo $x + $y.'<br>';
+    $x+=10; //  $x = $x + 10;
+    echo $x .'<br>';
+    $y+=10;
+    echo $y .'<br>';
+    $x++; //$x  +=1;
+    $x--; //$x  -=1;
+    echo M_PI.'<br>';
+    echo    M_E.'<br>';
+    ?>
+</body>
+</html>
